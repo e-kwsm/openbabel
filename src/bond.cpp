67f4f45a1382016e6a1db12a292505d72306c494
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2020-04-10 05:03:52 +0900

    src/bond.cpp

--- src/bond.cpp
+++ src/bond.cpp
@@ -531,14 +531,13 @@ namespace OpenBabel
   OBRing* OBBond::FindSmallestRing() const
   {
     vector<OBRing*> rlist;
-    vector<OBRing*>::iterator i;
 
     OBMol *mol = (OBMol*)((OBBond*)this)->GetParent();
 
     rlist = mol->GetSSSR();
     OBRing* result = (OBRing*) NULL;
     size_t min_size = UINT_MAX;
-    for (i = rlist.begin();i != rlist.end();++i) {
+    for (auto i = rlist.begin(); i != rlist.end(); ++i) {
       if ((*i)->IsMember((OBBond*)this) && (*i)->Size() < min_size) {
         min_size = (*i)->Size();
         result = *i;
