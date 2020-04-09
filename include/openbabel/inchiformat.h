88ebae496283a0d6a2961daa73b4850029547ec9
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2020-04-10 05:43:40 +0900

    include/openbabel/inchiformat.h

--- include/openbabel/inchiformat.h
+++ include/openbabel/inchiformat.h
@@ -136,10 +136,10 @@ public:
     bool operator()(const std::string& s1, const std::string& s2) const
     {
       //stop at the first space or the end of the strings
-      auto p1 = s1.begin();
-      auto p2 = s2.begin();
-      const auto p1end = find(s1.begin(), s1.end(), ' ');
-      const auto p2end = find(s2.begin(), s2.end(), ' ');
+      auto p1 = s1.cbegin();
+      auto p2 = s2.cbegin();
+      const auto p1end = find(s1.cbegin(), s1.cend(), ' ');
+      const auto p2end = find(s2.cbegin(), s2.cend(), ' ');
 
       while( p1<p1end && p2<p2end)
       {
