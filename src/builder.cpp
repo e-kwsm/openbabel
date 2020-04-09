615c715697d2d2742bbbe5a6c0486dd3f6515262
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2020-04-10 08:54:05 +0900

    src/builder.cpp

--- src/builder.cpp
+++ src/builder.cpp
@@ -1781,7 +1781,7 @@ namespace OpenBabel
             if (!b->IsInRing())
               reconnect.push_back(&*b);
 
-      for (auto bi=reconnect.begin(); bi!=reconnect.end(); ++bi) {
+      for (auto bi = reconnect.begin(); bi != reconnect.end(); ++bi) {
         OBBond* b = *bi;
         int bo = b->GetBondOrder();
         int begin = b->GetBeginAtomIdx();
