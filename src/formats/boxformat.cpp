6440385e4abf9f55d86b2daefb24bd91bffb9456
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2020-04-10 05:12:11 +0900

    src/formats/boxformat.cpp

--- src/formats/boxformat.cpp
+++ src/formats/boxformat.cpp
@@ -76,7 +76,6 @@ bool BoxFormat::ReadMolecule(OBBase* pOb, OBConversion* pConv)
 
     char buffer[BUFF_SIZE];
     vector<string> vs;
-    vector<string>::iterator i;
     OBAtom atom;
 
     mol.BeginModify();
@@ -100,7 +99,7 @@ bool BoxFormat::ReadMolecule(OBBase* pOb, OBConversion* pConv)
         {
             tokenize(vs,buffer);
             if (!vs.empty() && vs.size() > 2)
-                for (i = vs.begin(),i+=2;i != vs.end();++i)
+                for (auto i = vs.begin() + 2; i != vs.end(); ++i)
                     mol.AddBond(atoi(vs[1].c_str()),atoi((*i).c_str()),1);
         }
     }
