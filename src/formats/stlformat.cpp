4ed757e9beb7e63b6def5c026d1489f76bcc3f14
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2020-04-10 07:08:33 +0900

    src/formats/stlformat.cpp

--- src/formats/stlformat.cpp
+++ src/formats/stlformat.cpp
@@ -218,7 +218,7 @@ namespace OpenBabel
     tok2 = triangles.size();
     os.write( (const char*) &tok2, sizeof(uint32_t) );
 
-    for(vector<Triangle>::iterator i = triangles.begin(); i != triangles.end(); ++i) { // go through all triangles
+    for (auto i = triangles.begin(); i != triangles.end(); ++i) { // go through all triangles
       float nx,ny,nz;
       //		nx= (i->a.x + i->b.x + i->c.x) / 3.;
       //		ny= (i->a.y + i->b.y + i->c.y) / 3.;
