e998d6a6a1a0766d744f40a2f00913b988f20e59
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2020-04-10 14:21:52 +0900

    src/ops/opisomorph.cpp

--- src/ops/opisomorph.cpp
+++ src/ops/opisomorph.cpp
@@ -362,8 +362,6 @@ bool OpNewS::Do(OBBase* pOb, const char* OptionText, OpMap* pmap, OBConversion* 
 
   if(match && !inv && vec.size()>=2 && !vec[1].empty() && !nPatternAtoms)
   {
-    vector<vector<int> >::iterator iter;
-
     if (vec[1]=="extract" || (vec.size()>3 && vec[2]=="extract"))
     {
       //Delete all unmatched atoms. Use only the first match
@@ -375,15 +373,15 @@ bool OpNewS::Do(OBBase* pOb, const char* OptionText, OpMap* pmap, OBConversion* 
     // with multiple color parameters use the one corresponding to the query molecule, or the last
     if(imol>vec.size()-2)
       imol = vec.size()-2;
-    for(iter=pMappedAtoms->begin();iter!=pMappedAtoms->end();++iter)//each match
-       AddDataToSubstruct(pmol, *iter, "color", vec[imol+1]);
+    for (auto iter : *pMappedAtoms)//each match
+       AddDataToSubstruct(pmol, iter, "color", vec[imol+1]);
     return true;
   }
 
   if(pConv && pConv->IsLast())
   {
-    for(qiter=queries.begin();qiter!=queries.end();++qiter)
-      delete *qiter;
+    for (auto& qiter : queries)
+      delete qiter;
     queries.clear();
   }
   return true;
