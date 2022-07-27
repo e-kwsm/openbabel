2bfc2d541c4249c4ba60cb11ac4c624cd1a0f311
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2022-07-27 11:53:14 +0900

    src/formats/crystal09format.cpp

--- src/formats/crystal09format.cpp
+++ src/formats/crystal09format.cpp
@@ -173,25 +173,25 @@ bool Crystal09Format::ReadMolecule(OBBase* pOb, OBConversion* pConv)
       getline(ifs,line);
       tokenize(vs,line);
       if (vs.size() < 3) { pmol->EndModify(); return false; }
-      xvec.SetX(strtod((char*)vs[0].c_str(), nullptr));
-      yvec.SetX(strtod((char*)vs[1].c_str(), nullptr));
-      zvec.SetX(strtod((char*)vs[2].c_str(), nullptr));
+      xvec.SetX(stod(vs[0]));
+      yvec.SetX(stod(vs[1]));
+      zvec.SetX(stod(vs[2]));
 
       //Second Line to Parse use SetY
       getline(ifs,line);
       tokenize(vs,line);
       if (vs.size() < 3) { pmol->EndModify(); return false; }
-      xvec.SetY(strtod((char*)vs[0].c_str(), nullptr));
-      yvec.SetY(strtod((char*)vs[1].c_str(), nullptr));
-      zvec.SetY(strtod((char*)vs[2].c_str(), nullptr));
+      xvec.SetY(stod(vs[0]));
+      yvec.SetY(stod(vs[1]));
+      zvec.SetY(stod(vs[2]));
 
       //Third Line to Parse use SetZ
       getline(ifs,line);
       tokenize(vs,line);
       if (vs.size() < 3) { pmol->EndModify(); return false; }
-      xvec.SetZ(strtod((char*)vs[0].c_str(), nullptr));
-      yvec.SetZ(strtod((char*)vs[1].c_str(), nullptr));
-      zvec.SetZ(strtod((char*)vs[2].c_str(), nullptr));
+      xvec.SetZ(stod(vs[0]));
+      yvec.SetZ(stod(vs[1]));
+      zvec.SetZ(stod(vs[2]));
 
       //Declare a pointer for the UnitCell Data. Set the Unit Cell for
       //OBUnitCell and OBMol
@@ -224,8 +224,8 @@ bool Crystal09Format::ReadMolecule(OBBase* pOb, OBConversion* pConv)
           }
         }
         else {
-          tmp1 = strtod((char*)vs[3].c_str(), nullptr);
-          tmp2 = strtod((char*)vs[7].c_str(), nullptr);
+          tmp1 = stod(vs[3]);
+          tmp2 = stod(vs[7]);
           freq.push_back(tmp1);
           intensity.push_back(tmp2);
         }
