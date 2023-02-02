e7d6e89ea2fee7ec1df4bd2e418dbf6ef1bf83d5
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2023-02-02 15:49:48 +0900

    src/forcefields/forcefielduff.cpp src/ops/addnonpolarh.cpp src/ops/addpolarh.cpp src/ops/canonical.cpp src/ops/changecell.cpp src/ops/delpolarh.cpp

--- src/ops/addnonpolarh.cpp
+++ src/ops/addnonpolarh.cpp
@@ -40,7 +40,7 @@ OpAddNonPolarH theOpAddNonPolarH("AddNonPolarH"); //Global instance
 /////////////////////////////////////////////////////////////////
 bool OpAddNonPolarH::Do(OBBase* pOb, const char* OptionText, OpMap* pOptions, OBConversion* pConv)
 {
-  OBMol* pmol = dynamic_cast<OBMol*>(pOb);
+  auto* pmol = dynamic_cast<OBMol*>(pOb);
   if(!pmol)
     return false;
 
