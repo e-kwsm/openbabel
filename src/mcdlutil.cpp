78a086ba59d93d39c34b3bef762d76137d30d4fc
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-08-07 13:33:52 +0900

    src/mcdlutil.cpp

--- src/mcdlutil.cpp
+++ src/mcdlutil.cpp
@@ -1265,9 +1265,9 @@ namespace OpenBabel {
   }
 
   void TSimpleMolecule::clear() {
-    for (unsigned int i=0; i<fAtom.size(); i++) delete(fAtom.at(i));
+    for (auto & i : fAtom) delete i;
     fAtom.clear();
-    for (unsigned int i=0; i<fBond.size(); i++) delete(fBond.at(i));
+    for (auto & i : fBond) delete i;
     fBond.clear();
   };
 
@@ -3306,7 +3306,7 @@ namespace OpenBabel {
       };
 
     //freeing resorces
-    for (int i=0; i<aeqList.size(); i++) delete(aeqList.at(i));
+    for (auto & i : aeqList) delete i;
     aeqList.clear();
     free(bk);
     delete(em);
@@ -5184,8 +5184,8 @@ namespace OpenBabel {
   }
 
   void TemplateRedraw::clear() {
-    for (unsigned int i=0; i<queryData.size(); i++) {
-      delete(queryData[i]);
+    for (auto & i : queryData) {
+      delete i;
     };
   };
 
@@ -7164,8 +7164,8 @@ namespace OpenBabel {
         };
       sm.redraw(atomList,bondList,atomList.size(),nb,3,atEx,bondN,false);
       //Setting OBMOL coordinates
-      for (int i=0; i<atomList.size(); i++) {
-        n=atomList[i];
+      for (int i : atomList) {
+        n=i;
         atom=pmol->GetAtom(n+1);  //1-based
         atom->SetVector(sm.getAtom(n)->rx,sm.getAtom(n)->ry,0.0);
       };
