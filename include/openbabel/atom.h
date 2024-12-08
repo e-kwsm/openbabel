047e62fba634c04765870e60e22adfc50303e975
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-12-09 07:03:13 +0900

    fixup! include/openbabel/atom.h

--- include/openbabel/atom.h
+++ include/openbabel/atom.h
@@ -278,26 +278,26 @@ namespace OpenBabel
       // or an internal vector (e.g., SetCoordPtr())
       //! \return the x coordinate
       double      x() const {
-        if (_c) {          return((*_c)[_cidx]); }
-        else    {          return _v.x(); }
+        if (_c)          { return((*_c)[_cidx]); }
+        else             { return _v.x(); }
       }
       //! \return the y coordinate
       double      y() const {
-        if (_c) {          return((*_c)[_cidx+1]); }
-        else    {          return _v.y(); }
+        if (_c)          { return((*_c)[_cidx+1]); }
+        else             { return _v.y(); }
       }
       //! \return the z coordinate
       double      z() const {
-        if (_c) {          return((*_c)[_cidx+2]); }
-        else    {          return _v.z(); }
+        if (_c)          { return((*_c)[_cidx+2]); }
+        else             { return _v.z(); }
       }
       //! \return the coordinates as a double* or NULL if none.
       //!
       //! See SetCoordPtr() for more. If no coordinate pointer is used
       //! (e.g., only vector3), NULL will be returned.
       double     *GetCoordinate(){
-        if (_c) {        return(&(*_c)[_cidx]); }
-        else    {        return nullptr; }
+        if (_c)        { return(&(*_c)[_cidx]); }
+        else           { return nullptr; }
       }
       //! \return the coordinates as a vector3 object
       vector3   &GetVector();
