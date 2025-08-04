892de3d21035811e8b8cd85311343284aa88972a
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-08-04 17:38:12 +0900

    src/forcefields/forcefieldgaff.cpp

--- src/forcefields/forcefieldgaff.cpp
+++ src/forcefields/forcefieldgaff.cpp
@@ -75,19 +75,19 @@ namespace OpenBabel
       OBFFLog("------------------------------------------------------------------------\n");
     }
 
-    for (vector<OBFFBondCalculationGaff>::iterator i = _bondcalculations.begin(); i != _bondcalculations.end(); ++i) {
+    for (auto& i : _bondcalculations) {
 
-      i->template Compute<gradients>();
-      energy += i->energy;
+      i.template Compute<gradients>();
+      energy += i.energy;
 
       if (gradients) {
-        AddGradient((*i).force_a, (*i).idx_a);
-        AddGradient((*i).force_b, (*i).idx_b);
+        AddGradient(i.force_a, i.idx_a);
+        AddGradient(i.force_b, i.idx_b);
       }
 
       IF_OBFF_LOGLVL_HIGH {
-        snprintf(_logbuf, BUFF_SIZE, "%s %s  %8.3f   %8.3f     %8.3f   %8.3f   %8.3f\n", (*i).a->GetType(), (*i).b->GetType(),
-                (*i).rab, (*i).r0, (*i).kr, (*i).delta, (*i).energy);
+        snprintf(_logbuf, BUFF_SIZE, "%s %s  %8.3f   %8.3f     %8.3f   %8.3f   %8.3f\n", i.a->GetType(), i.b->GetType(),
+                i.rab, i.r0, i.kr, i.delta, i.energy);
         OBFFLog(_logbuf);
       }
     }
@@ -143,20 +143,20 @@ namespace OpenBabel
       OBFFLog("-----------------------------------------------------------------------------\n");
     }
 
-    for (vector<OBFFAngleCalculationGaff>::iterator i = _anglecalculations.begin(); i != _anglecalculations.end(); ++i) {
+    for (auto& i : _anglecalculations) {
 
-      i->template Compute<gradients>();
-      energy += i->energy;
+      i.template Compute<gradients>();
+      energy += i.energy;
 
       if (gradients) {
-        AddGradient((*i).force_a, (*i).idx_a);
-        AddGradient((*i).force_b, (*i).idx_b);
-        AddGradient((*i).force_c, (*i).idx_c);
+        AddGradient(i.force_a, i.idx_a);
+        AddGradient(i.force_b, i.idx_b);
+        AddGradient(i.force_c, i.idx_c);
       }
 
       IF_OBFF_LOGLVL_HIGH {
-        snprintf(_logbuf, BUFF_SIZE, "%s %s %s  %8.3f   %8.3f     %8.3f   %8.3f   %8.3f\n", (*i).a->GetType(), (*i).b->GetType(),
-                (*i).c->GetType(), (*i).theta, (*i).theta0, (*i).kth, (*i).delta, (*i).energy);
+        snprintf(_logbuf, BUFF_SIZE, "%s %s %s  %8.3f   %8.3f     %8.3f   %8.3f   %8.3f\n", i.a->GetType(), i.b->GetType(),
+                i.c->GetType(), i.theta, i.theta0, i.kth, i.delta, i.energy);
         OBFFLog(_logbuf);
       }
     }
@@ -214,21 +214,21 @@ namespace OpenBabel
       OBFFLog("----------------------------------------------------------------\n");
     }
 
-    for (vector<OBFFTorsionCalculationGaff>::iterator i = _torsioncalculations.begin(); i != _torsioncalculations.end(); ++i) {
+    for (auto& i : _torsioncalculations) {
 
-      i->template Compute<gradients>();
-      energy += i->energy;
+      i.template Compute<gradients>();
+      energy += i.energy;
 
       if (gradients) {
-        AddGradient((*i).force_a, (*i).idx_a);
-        AddGradient((*i).force_b, (*i).idx_b);
-        AddGradient((*i).force_c, (*i).idx_c);
-        AddGradient((*i).force_d, (*i).idx_d);
+        AddGradient(i.force_a, i.idx_a);
+        AddGradient(i.force_b, i.idx_b);
+        AddGradient(i.force_c, i.idx_c);
+        AddGradient(i.force_d, i.idx_d);
       }
 
       IF_OBFF_LOGLVL_HIGH {
-        snprintf(_logbuf, BUFF_SIZE, "%s %s %s %s    %6.3f    %5.0f   %8.3f   %1.0f   %8.3f\n", (*i).a->GetType(), (*i).b->GetType(),
-                (*i).c->GetType(), (*i).d->GetType(), (*i).vn_half, (*i).gamma, (*i).tor, (*i).n, (*i).energy);
+        snprintf(_logbuf, BUFF_SIZE, "%s %s %s %s    %6.3f    %5.0f   %8.3f   %1.0f   %8.3f\n", i.a->GetType(), i.b->GetType(),
+                i.c->GetType(), i.d->GetType(), i.vn_half, i.gamma, i.tor, i.n, i.energy);
         OBFFLog(_logbuf);
       }
     }
@@ -315,21 +315,21 @@ namespace OpenBabel
       OBFFLog("----------------------------------------------------------------\n");
     }
 
-    for (vector<OBFFOOPCalculationGaff>::iterator i = _oopcalculations.begin(); i != _oopcalculations.end(); ++i) {
+    for (auto& i : _oopcalculations) {
 
-      i->template Compute<gradients>();
-      energy += i->energy;
+      i.template Compute<gradients>();
+      energy += i.energy;
 
       if (gradients) {
-        AddGradient((*i).force_a, (*i).idx_a);
-        AddGradient((*i).force_b, (*i).idx_b);
-        AddGradient((*i).force_c, (*i).idx_c);
-        AddGradient((*i).force_d, (*i).idx_d);
+        AddGradient(i.force_a, i.idx_a);
+        AddGradient(i.force_b, i.idx_b);
+        AddGradient(i.force_c, i.idx_c);
+        AddGradient(i.force_d, i.idx_d);
       }
 
       IF_OBFF_LOGLVL_HIGH {
-        snprintf(_logbuf, BUFF_SIZE, "%s %s %s %s    %6.3f    %5.0f   %8.3f   %1.0f   %8.3f\n", (*i).a->GetType(), (*i).b->GetType(),
-                (*i).c->GetType(), (*i).d->GetType(), (*i).vn_half, (*i).gamma, (*i).tor, (*i).n, (*i).energy);
+        snprintf(_logbuf, BUFF_SIZE, "%s %s %s %s    %6.3f    %5.0f   %8.3f   %1.0f   %8.3f\n", i.a->GetType(), i.b->GetType(),
+                i.c->GetType(), i.d->GetType(), i.vn_half, i.gamma, i.tor, i.n, i.energy);
         OBFFLog(_logbuf);
       }
     }
@@ -1245,11 +1245,11 @@ namespace OpenBabel
       }
     }
 
-    for (auto i = _vexttyp.begin(); i != _vexttyp.end(); ++i) {
-      if (i->first->Match(_mol)) {
-        _mlist = i->first->GetMapList();
-        for (vector<vector<int> >::iterator j = _mlist.begin();j != _mlist.end();++j) {
-          _mol.GetAtom((*j)[0])->SetType(i->second);
+    for (const auto& i : _vexttyp) {
+      if (i.first->Match(_mol)) {
+        _mlist = i.first->GetMapList();
+        for (const auto& j : _mlist) {
+          _mol.GetAtom(j[0])->SetType(i.second);
         }
       }
     }
