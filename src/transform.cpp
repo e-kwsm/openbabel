a4c5aa0d34c60271ce704fe00016b2a6f7b815dd
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-08-04 16:22:16 +0900

    XXX:src/transform.cpp

--- src/transform.cpp
+++ src/transform.cpp
@@ -125,9 +125,8 @@ namespace OpenBabel
         string txt(itr->second);
 
         vector<string> vec;
-        vector<string>::iterator it;
         tokenize(vec, txt,";");
-        for(it=vec.begin();it!=vec.end();++it) {
+        for(vector<string>::iterator it=vec.begin();it!=vec.end();++it) {
           string attr, val;
           string::size_type pos = it->find('=');
           if(pos==string::npos) {
