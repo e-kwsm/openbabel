e56ac5acbc595a2ed3e099edfe578a79748a62bd
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-08-04 17:09:12 +0900

    XXX:src/formats/cifformat.cpp

--- src/formats/cifformat.cpp
+++ src/formats/cifformat.cpp
@@ -334,8 +334,8 @@ namespace OpenBabel
         if(mvItem.find("_cell_length_a"   )!=mvItem.end()) empty_iucrjournal_block=false;
         if(mvItem.find("_cell_length_b"   )!=mvItem.end()) empty_iucrjournal_block=false;
         if(mvItem.find("_cell_length_c"   )!=mvItem.end()) empty_iucrjournal_block=false;
-        for(map<set<ci_string>,map<ci_string,vector<string> > >::const_iterator loop=mvLoop.begin();
-            loop!=mvLoop.end();++loop)
+        for(map<set<ci_string>,map<ci_string,vector<string> > >::const_iterator loop=mvLoop.cbegin();
+            loop!=mvLoop.cend();++loop)
           {
             if(loop->second.find("_atom_site_fract_x")!=loop->second.end()) empty_iucrjournal_block=false;
             if(loop->second.find("_atom_site_fract_y")!=loop->second.end()) empty_iucrjournal_block=false;
@@ -632,8 +632,8 @@ namespace OpenBabel
           found = true;
         }
       else {
-        for(map<set<ci_string>,map<ci_string,vector<string> > >::const_iterator loop=mvLoop.begin();
-            loop!=mvLoop.end();++loop)
+        for(map<set<ci_string>,map<ci_string,vector<string> > >::const_iterator loop=mvLoop.cbegin();
+            loop!=mvLoop.cend();++loop)
           {
             map<ci_string,vector<string> >::const_iterator pos;
             unsigned i, nb;
@@ -743,8 +743,8 @@ namespace OpenBabel
   void CIFData::ExtractAtomicPositions()
   {
     map<ci_string,string>::const_iterator positem;
-    for(map<set<ci_string>,map<ci_string,vector<string> > >::const_iterator loop=mvLoop.begin();
-        loop!=mvLoop.end();++loop)
+    for(map<set<ci_string>,map<ci_string,vector<string> > >::const_iterator loop=mvLoop.cbegin();
+        loop!=mvLoop.cend();++loop)
       {
         if(mvAtom.size()>0) break;// only extract ONE list of atoms, preferably fractional coordinates
         map<ci_string,vector<string> >::const_iterator posx,posy,posz,poslabel,possymbol,posoccup;
@@ -846,7 +846,7 @@ namespace OpenBabel
   void CIFData::ExtractBonds()
   {
     map<ci_string,string>::const_iterator positem;
-    for(map<set<ci_string>,map<ci_string,vector<string> > >::const_iterator loop=mvLoop.begin(); loop!=mvLoop.end();++loop)
+    for(map<set<ci_string>,map<ci_string,vector<string> > >::const_iterator loop=mvLoop.cbegin(); loop!=mvLoop.cend();++loop)
       {
         //if(mvBond.size()>0) break;// Only allow one bond list
         map<ci_string,vector<string> >::const_iterator poslabel1,poslabel2,posdist;
@@ -876,7 +876,7 @@ namespace OpenBabel
     map<ci_string,string>::const_iterator positem;
 
     map<std::string, double> lbl2ox;
-    for(map<set<ci_string>, map<ci_string, vector<string> > >::const_iterator loop=mvLoop.begin(); loop!=mvLoop.end(); ++loop)
+    for(map<set<ci_string>, map<ci_string, vector<string> > >::const_iterator loop=mvLoop.cbegin(); loop!=mvLoop.cend(); ++loop)
     {
       //if(mvBond.size()>0) break;// Only allow one bond list
       map<ci_string,vector<string> >::const_iterator pos_symbol, pos_ox_number, posdist;
