85c5b6d9d97ddd0eb50b339f6cf7ce671af1c714
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-08-04 20:03:01 +0900

    fixup! XXX:src/mol.cpp

--- src/mol.cpp
+++ src/mol.cpp
@@ -2929,7 +2929,7 @@ namespace OpenBabel
       return;
 
     OBBitVec bv;
-    for (i = va.begin();i != va.end();++i)
+    for (vector<OBAtom*>::iterator i = va.begin();i != va.end();++i)
       bv |= (*i)->GetIdx();
 
     for (atom = BeginAtom(i);atom;atom = NextAtom(i))
@@ -3271,14 +3271,13 @@ namespace OpenBabel
     //         (set all atoms with at least two bonds to sp2)
 
     vector<OBRing*> rlist;
-    vector<OBRing*>::iterator ringit;
     vector<int> path;
     double torsions = 0.0;
 
     if (!HasSSSRPerceived())
       FindSSSR();
     rlist = GetSSSR();
-    for (ringit = rlist.begin(); ringit != rlist.end(); ++ringit)
+    for (vector<OBRing*>::iterator ringit = rlist.begin(); ringit != rlist.end(); ++ringit)
       {
         if ((*ringit)->Size() == 5)
           {
@@ -3368,7 +3367,7 @@ namespace OpenBabel
     bool needs_kekulization = false; // are there any aromatic bonds?
     bool typed; // has this ring been typed?
     unsigned int loop, loopSize;
-    for (ringit = rlist.begin(); ringit != rlist.end(); ++ringit)
+    for (vector<OBRing*>::iterator ringit = rlist.begin(); ringit != rlist.end(); ++ringit)
       {
         typed = false;
         loopSize = (*ringit)->Size();
@@ -3733,8 +3732,7 @@ namespace OpenBabel
 
   void OBMol::SetConformers(vector<double*> &v)
   {
-    vector<double*>::iterator i;
-    for (i = _vconf.begin();i != _vconf.end();++i)
+    for (vector<double*>::iterator i = _vconf.begin();i != _vconf.end();++i)
       delete [] *i;
 
     _vconf = v;
@@ -4026,10 +4024,9 @@ namespace OpenBabel
     vector<OBRing*> vr;
     vr = GetLSSR();
 
-    vector<OBRing*>::iterator i;
     vector<int>::iterator j;
 
-    for (i = vr.begin();i != vr.end();++i) {
+    for (vector<OBRing*>::iterator i = vr.begin();i != vr.end();++i) {
       a_in = false;
       b_in = false;
       // Go through the path of the ring and see if a and/or b match
