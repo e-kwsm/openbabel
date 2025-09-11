9d2b214185136b4687ab70f2e9a9a5d7413b7f9b
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-09-12 06:02:06 +0900

    src/atom.cpp

--- src/atom.cpp
+++ src/atom.cpp
@@ -1021,12 +1021,12 @@ namespace OpenBabel
   {
     // TODO: Is this data stored elsewhere?
     // The number of valence electrons in a free atom
-    const int VALENCE[113] = {0,1,2,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,9,10,
+    const std::array<int, 113> VALENCE = {0,1,2,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,9,10,
                               11,12,3,4,5,6,7,8,1,2,3,4,5,6,7,8,9,10,11,12,3,4,5,6,7,8,1,2,
                               4,4,4,4,4,4,4,4,4,4,4,4,4,4,3,4,5,6,7,8,9,10,11,12,3,4,5,6,7,
                               8,1,1,4,4,4,4,4,4,4,4,4,4,4,4,4,4,3,4,5,6,7,8,9,10,11,12};
     // The number of electrons required to make up a full valence shell
-    const int SHELL[113]   = {0,2,2,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,18,18,18,18,18,18,
+    const std::array<int, 113> SHELL   = {0,2,2,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,18,18,18,18,18,18,
                               18,18,18,18,8,8,8,8,8,8,8,8,18,18,18,18,18,18,18,18,18,18,8,
                               8,8,8,8,8,8,8,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,
                               18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,8,8,18,18,18,18,
@@ -1872,12 +1872,12 @@ namespace OpenBabel
   bool OBAtom::IsMetal()
   {
     const unsigned NMETALS = 78;
-    const int metals[NMETALS] = {
+    const std::array<int, NMETALS> metals = {
     3,4,11,12,13,19,20,21,22,23,24,25,26,27,28,29,
     30,31,37,38,39,40,41,42,43,44,45,46,47,48,49,50,55,56,57,58,59,60,61,62,63,
     64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,87,88,89,90,91,
     92,93,94,95,96,97,98,99,100,101,102,103};
-    return std::find(metals, metals+78, GetAtomicNum())!=metals+78;
+    return std::find(metals.begin(), metals.end(), GetAtomicNum()) != metals.end();
   }
 
 } // end namespace OpenBabel
