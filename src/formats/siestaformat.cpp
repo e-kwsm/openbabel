266e2202dc1edbbee96112070d50f1e9d52a9509
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-09-13 07:25:08 +0900

    src/formats/siestaformat.cpp

--- src/formats/siestaformat.cpp
+++ src/formats/siestaformat.cpp
@@ -23,7 +23,7 @@ GNU General Public License for more details.
 #include <cstdlib>
 
 
-#define EV_TO_KCAL_PER_MOL 23.060538
+const double EV_TO_KCAL_PER_MOL = 23.060538;
 
 using namespace std;
 namespace OpenBabel {
