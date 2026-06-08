c162ad99e8993f5d2b9eb80655a6b55369ceff0e
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2026-06-08 10:43:03 +0900

    fixup! readability-redundant-string-init
    Signed-off-by: Eisuke Kawashima <e-kwsm@users.noreply.github.com>

--- src/formats/mdffformat.cpp
+++ src/formats/mdffformat.cpp
@@ -103,7 +103,7 @@ namespace OpenBabel {
     int atom_etab_num;
     string atom_symbol;
     double atom_charge;
-    atm_t_prop(): num_of_atoms(0), atom_symbol(""), atom_charge(0.0) {};
+    atm_t_prop(): num_of_atoms(0), atom_charge(0.0) {};
   };
   
 
