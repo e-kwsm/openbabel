739cff7c2f65da9127ed4b36ce4bf425ec4c0bce
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2026-08-22 21:55:55 +0900

    external/inchi-patch.cmake

--- external/inchi-patch.cmake
+++ external/inchi-patch.cmake
@@ -2,7 +2,4 @@
 
 # InChI does not have install target
 file(APPEND "${ORIG}" "
-# add_custom_command(TARGET libinchi POST_BUILD
-#   COMMAND \"\${CMAKE_COMMAND}\" -E copy \$<TARGET_FILE:libinchi> \"${PROJECT_BINARY_DIR}/${LIB_INSTALL_DIR}\"
-# )
 install(TARGETS libinchi LIBRARY DESTINATION ${LIB_INSTALL_DIR})")
