1826b1c4866db174d77f28e6912c4b656f3bee1e
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2026-08-22 22:30:41 +0900

    external/inchi-patch.cmake

--- external/inchi-patch.cmake
+++ external/inchi-patch.cmake
@@ -3,8 +3,10 @@
 # InChI does not have install target
 file(APPEND "${ORIG}" "
 ### OPENBABEL PATCH ###
+file(GLOB inchi_headers \"\${PROJECT_SOURCE_DIR}/../../../INCHI_BASE/src/*.h\")
 install(FILES
-\"\${PROJECT_SOURCE_DIR}/../../../INCHI_BASE/src/inchi_api.h\"
-\"\${PROJECT_SOURCE_DIR}/../../../INCHI_BASE/src/bcf_s.h\"
+# \"\${PROJECT_SOURCE_DIR}/../../../INCHI_BASE/src/inchi_api.h\"
+# \"\${PROJECT_SOURCE_DIR}/../../../INCHI_BASE/src/bcf_s.h\"
+\${inchi_headers}
 DESTINATION include/inchi)
 install(TARGETS libinchi LIBRARY DESTINATION ${LIB_INSTALL_DIR})")
