da355cc3e1093902be0843f67234d7845466cf64
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2026-08-23 05:36:27 +0900

    external/inchi.cmake

--- external/inchi.cmake
+++ external/inchi.cmake
@@ -3,8 +3,10 @@ if(NOT OPENBABEL_USE_SYSTEM_INCHI)
     URL https://github.com/IUPAC-InChI/InChI/archive/refs/tags/v1.07.5.tar.gz
     URL_HASH SHA256=9a8af985295c47bfaf424ad8386b1597da515589665ec71908dae1bd2b67ac96
     SOURCE_SUBDIR INCHI-1-SRC/INCHI_API/libinchi/src
+    FIND_PACKAGE_ARGS NAMES Inchi
   )
   FetchContent_MakeAvailable(inchi)
-  add_custom_target(hoge COMMAND "${CMAKE_COMMAND}" -E copy )
+  FetchContent_GetProperties(inchi BINARY_DIR inchi_bin)
+  add_custom_target(hoge COMMAND "${CMAKE_COMMAND}" -E false copy ${inchi_bin}/${LIB_INSTALL_DIR}/libinchi.so ${PROJECT_BINARY_DIR}/${LIB_INSTALL_DIR})
 #   COMMAND \"\${CMAKE_COMMAND}\" -E copy \$<TARGET_FILE:libinchi> \"${PROJECT_BINARY_DIR}/${LIB_INSTALL_DIR}\"
 endif()
